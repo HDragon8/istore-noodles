f4e971563915b7bad8cc1a499b386adb6e6beb56
HDragon <17612975+HDragon8@users.noreply.github.com>
2024-12-28 22:34:01 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -28,6 +28,23 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && 
 git clone --depth 1 https://github.com/kenzok8/small && mvdir small
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages && mvdir openwrt-packages
 
+git clone --depth 1 https://github.com/tty228/luci-app-wechatpush
+git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
+git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
+git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
+git clone --depth 1 https://github.com/sirpdboy/luci-app-lucky lucik && mv -n lucik/luci-app-lucky ./ ; rm -rf lucik
+
+git clone --depth 1 https://github.com/linkease/nas-packages && mv -n nas-packages/{network/services/*,multimedia/*} ./; rm -rf nas-packages
+git clone --depth 1 https://github.com/linkease/nas-packages-luci && mv -n nas-packages-luci/luci/* ./; rm -rf nas-packages-luci
+git clone --depth 1 https://github.com/linkease/istore && mv -n istore/luci/* ./; rm -rf istore
+git clone --depth 1 https://github.com/linkease/openwrt-app-actions && mv -n openwrt-app-actions/applications/* ./;rm -rf openwrt-app-actions
+git clone --depth 1 https://github.com/Lienol/openwrt-package && mv -n openwrt-package/{luci-app-ramfree,luci-app-timecontrol} ./; rm -rf openwrt-package
+
+svn export https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-autoreboot
+
+git_sparse_clone master "https://github.com/immortalwrt/luci" "immluci1" applications/luci-app-timewol applications/luci-app-dufs
+git_sparse_clone master "https://github.com/immortalwrt/packages" "impaks" net/dufs 
+
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
 
